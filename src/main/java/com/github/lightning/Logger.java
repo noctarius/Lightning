17c2a62cbfc800393f94e5353b7b3089b3c9fe90
Noctarius <me@noctarius.com>
2012-03-16 08:01:54 +0100

    Added a logger interface to adapt logging to other logging frameworks

---
+++ src/main/java/com/github/lightning/Logger.java
@@ -0,0 +1,52 @@
+package com.github.lightning;
+
+public interface Logger {
+
+	public static enum LogLevel {
+		Trace,
+		Debug,
+		Info,
+		Warn,
+		Error,
+		Fatal
+	}
+
+	boolean isLogLevelEnabled(LogLevel logLevel);
+
+	boolean isTraceEnabled();
+
+	boolean isDebugEnabled();
+
+	boolean isInfoEnabled();
+
+	boolean isWarnEnabled();
+
+	boolean isErrorEnabled();
+
+	boolean isFatalEnabled();
+
+	void trace(String message);
+
+	void trace(String message, Throwable throwable);
+
+	void debug(String message);
+
+	void debug(String message, Throwable throwable);
+
+	void info(String message);
+
+	void info(String message, Throwable throwable);
+
+	void warn(String message);
+
+	void warn(String message, Throwable throwable);
+
+	void error(String message);
+
+	void error(String message, Throwable throwable);
+
+	void fatal(String message);
+
+	void fatal(String message, Throwable throwable);
+
+}
---
+++ src/main/java/com/github/lightning/base/LoggerAdapter.java
@@ -0,0 +1,89 @@
+package com.github.lightning.base;
+
+import com.github.lightning.Logger;
+
+public class LoggerAdapter implements Logger {
+
+	@Override
+	public boolean isLogLevelEnabled(LogLevel logLevel) {
+		return false;
+	}
+
+	@Override
+	public boolean isTraceEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isDebugEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isInfoEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isWarnEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isErrorEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isFatalEnabled() {
+		return false;
+	}
+
+	@Override
+	public void trace(String message) {
+	}
+
+	@Override
+	public void trace(String message, Throwable throwable) {
+	}
+
+	@Override
+	public void debug(String message) {
+	}
+
+	@Override
+	public void debug(String message, Throwable throwable) {
+	}
+
+	@Override
+	public void info(String message) {
+	}
+
+	@Override
+	public void info(String message, Throwable throwable) {
+	}
+
+	@Override
+	public void warn(String message) {
+	}
+
+	@Override
+	public void warn(String message, Throwable throwable) {
+	}
+
+	@Override
+	public void error(String message) {
+	}
+
+	@Override
+	public void error(String message, Throwable throwable) {
+	}
+
+	@Override
+	public void fatal(String message) {
+	}
+
+	@Override
+	public void fatal(String message, Throwable throwable) {
+	}
+}
