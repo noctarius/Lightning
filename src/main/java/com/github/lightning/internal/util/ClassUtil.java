d1374f0d8ab4e4fd0798d69fe8744c37d1322b69
noctarius <me@noctarius.com>
2012-03-20 14:59:58 +0100

    Added calculation for SerialVersionUID

--- src/main/java/com/github/lightning/internal/util/ClassUtil.java
+++ src/main/java/com/github/lightning/internal/util/ClassUtil.java
@@ -15,13 +15,33 @@
  */
 package com.github.lightning.internal.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectStreamClass;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 public final class ClassUtil {
+
+	private static final Map<Class<?>, Long> SERIAL_VERSION_UID_CACHE = new ConcurrentHashMap<Class<?>, Long>();
 
 	private ClassUtil() {
 	}
@@ -75,8 +95,131 @@ public final class ClassUtil {
 		throw new ClassNotFoundException("Class " + canonicalName + " not found on classpath");
 	}
 
+	@SuppressWarnings("unchecked")
 	public static long calculateSerialVersionUID(Class<?> clazz) {
-		return ObjectStreamClass.lookupAny(clazz).getSerialVersionUID();
+		Long serialVersionUID = SERIAL_VERSION_UID_CACHE.get(clazz);
+		if (serialVersionUID != null) {
+			return serialVersionUID;
+		}
+
+		if (Serializable.class.isAssignableFrom(clazz)) {
+			serialVersionUID = ObjectStreamClass.lookup(clazz).getSerialVersionUID();
+			SERIAL_VERSION_UID_CACHE.put(clazz, serialVersionUID);
+			return serialVersionUID;
+		}
+
+		serialVersionUID = getSerialVersionUIDFromField(clazz);
+		if (serialVersionUID != null) {
+			SERIAL_VERSION_UID_CACHE.put(clazz, serialVersionUID);
+			return serialVersionUID;
+		}
+
+		try {
+			ClassReader reader = new ClassReader(Type.getInternalName(clazz).replace("/", "."));
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataOutputStream out = new DataOutputStream(baos);
+
+			ClassNode classNode = new ClassNode();
+			reader.accept(classNode, 0);
+
+			// Classname
+			out.writeUTF(toJavaName(classNode.name));
+
+			// Modifiers
+			out.writeInt(clazz.getModifiers() & (Modifier.PUBLIC | Modifier.FINAL | Modifier.INTERFACE | Modifier.ABSTRACT));
+
+			// Interfaces
+			Collections.sort(classNode.interfaces);
+			for (int i = 0; i < classNode.interfaces.size(); i++) {
+				out.writeUTF(toJavaName((String) classNode.interfaces.get(i)));
+			}
+
+			// Fields
+			Field[] fields = clazz.getDeclaredFields();
+			Arrays.sort(fields, new Comparator<Field>() {
+
+				@Override
+				public int compare(Field o1, Field o2) {
+					return o1.getName().compareTo(o2.getName());
+				}
+			});
+
+			for (Field field : fields) {
+				int mods = field.getModifiers();
+				if (((mods & Modifier.PRIVATE) == 0 || (mods & (Modifier.STATIC | Modifier.TRANSIENT)) == 0)) {
+					out.writeUTF(field.getName());
+					out.writeInt(mods);
+					out.writeUTF(Type.getDescriptor(field.getType()));
+				}
+			}
+
+			// Static Initializer
+			if (getStaticInitializer(classNode) != null) {
+				out.writeUTF("<clinit>");
+				out.writeInt(Modifier.STATIC);
+				out.writeUTF("()V");
+			}
+
+			// Constructors
+			Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+			Arrays.sort(constructors, new Comparator<Constructor<?>>() {
+
+				@Override
+				public int compare(Constructor<?> o1, Constructor<?> o2) {
+					return Type.getConstructorDescriptor(o1).compareTo(Type.getConstructorDescriptor(o2));
+				}
+			});
+
+			for (int i = 0; i < constructors.length; i++) {
+				Constructor<?> constructor = constructors[i];
+				int mods = constructor.getModifiers();
+				if ((mods & Modifier.PRIVATE) == 0) {
+					out.writeUTF("<init>");
+					out.writeInt(mods);
+					out.writeUTF(toJavaName(Type.getConstructorDescriptor(constructor)));
+				}
+			}
+
+			// Methods
+			Method[] methods = clazz.getDeclaredMethods();
+			Arrays.sort(methods, new Comparator<Method>() {
+
+				@Override
+				public int compare(Method o1, Method o2) {
+					return Type.getMethodDescriptor(o1).compareTo(Type.getMethodDescriptor(o2));
+				}
+			});
+
+			for (int i = 0; i < methods.length; i++) {
+				Method method = methods[i];
+				int mods = method.getModifiers();
+				if ((mods & Modifier.PRIVATE) == 0) {
+					out.writeUTF("<init>");
+					out.writeInt(mods);
+					out.writeUTF(toJavaName(Type.getMethodDescriptor(method)));
+				}
+			}
+
+			// Final calculation
+			out.flush();
+			MessageDigest digest = MessageDigest.getInstance("SHA");
+			byte[] checksum = digest.digest(baos.toByteArray());
+
+			long hash = 0;
+			for (int i = Math.min(checksum.length, 8) - 1; i >= 0; i--) {
+				hash = (hash << 8) | (checksum[i] & 0xFF);
+			}
+
+			SERIAL_VERSION_UID_CACHE.put(clazz, hash);
+			return hash;
+		}
+		catch (IOException e) {
+		}
+		catch (NoSuchAlgorithmException e) {
+		}
+
+		return -1L;
 	}
 
 	public static byte[] getClassBytes(Class<?> clazz) {
@@ -93,4 +236,31 @@ public final class ClassUtil {
 		}
 	}
 
+	private static String toJavaName(String classname) {
+		return classname.replace("/", ".");
+	}
+
+	private static Long getSerialVersionUIDFromField(Class<?> clazz) {
+		try {
+			Field f = clazz.getDeclaredField("serialVersionUID");
+			int mask = Modifier.STATIC | Modifier.FINAL;
+			if ((f.getModifiers() & mask) == mask) {
+				f.setAccessible(true);
+				return Long.valueOf(f.getLong(null));
+			}
+		}
+		catch (Exception ex) {
+		}
+		return null;
+	}
+
+	private static MethodNode getStaticInitializer(ClassNode classNode) {
+		for (Object method : classNode.methods) {
+			MethodNode methodNode = (MethodNode) method;
+			if ("<clinit>".equals(methodNode.name) && ((methodNode.access & Opcodes.ACC_STATIC) != 0)) {
+				return methodNode;
+			}
+		}
+		return null;
+	}
 }
