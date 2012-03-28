612a51ab953ffde692bfb58d5362be65c3cdd89a
noctarius <me@noctarius.com>
2012-03-28 09:15:32 +0200

    Added Spring implementation

---
+++ lightning-integration/lightning-integration-spring/src/main/java/com/github/lightning/spring/LightningFactoryBean.java
@@ -0,0 +1,120 @@
+package com.github.lightning.spring;
+
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.github.lightning.ClassComparisonStrategy;
+import com.github.lightning.Lightning;
+import com.github.lightning.SerializationStrategy;
+import com.github.lightning.Serializer;
+import com.github.lightning.configuration.SerializerDefinition;
+import com.github.lightning.logging.Logger;
+import com.github.lightning.metadata.Attribute;
+
+public class LightningFactoryBean implements FactoryBean, InitializingBean {
+
+	private Serializer singletonSerializer;
+
+	private boolean prototype = false;
+	private List<SerializerDefinition> serializerDefinitions = new ArrayList<SerializerDefinition>();
+	private ClassComparisonStrategy classComparisonStrategy = ClassComparisonStrategy.LightningChecksum;
+	private Class<? extends Annotation> attributesAnnotation = Attribute.class;
+	private SerializationStrategy serializationStrategy = SerializationStrategy.SpeedOptimized;
+	private Logger logger = new SpringLoggingAdapter();
+	private File debugCacheDirectory = null;
+
+	@Override
+	public Object getObject() throws Exception {
+		if (isSingleton()) {
+			return singletonSerializer;
+		}
+
+		return Lightning.newBuilder().classComparisonStrategy(classComparisonStrategy)
+				.debugCacheDirectory(debugCacheDirectory).describesAttributs(attributesAnnotation)
+				.logger(logger).serializationStrategy(serializationStrategy)
+				.serializerDefinitions(serializerDefinitions).build();
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Serializer.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return !prototype;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (isSingleton()) {
+			synchronized (this) {
+				singletonSerializer = Lightning.newBuilder().classComparisonStrategy(classComparisonStrategy)
+						.debugCacheDirectory(debugCacheDirectory).describesAttributs(attributesAnnotation)
+						.logger(logger).serializationStrategy(serializationStrategy)
+						.serializerDefinitions(serializerDefinitions).build();
+			}
+		}
+	}
+
+	public void setPrototype(boolean prototype) {
+		this.prototype = prototype;
+	}
+
+	public boolean getPrototype() {
+		return prototype;
+	}
+
+	public List<SerializerDefinition> getSerializerDefinitions() {
+		return serializerDefinitions;
+	}
+
+	public void setSerializerDefinitions(List<SerializerDefinition> serializerDefinitions) {
+		this.serializerDefinitions = serializerDefinitions;
+	}
+
+	public ClassComparisonStrategy getClassComparisonStrategy() {
+		return classComparisonStrategy;
+	}
+
+	public void setClassComparisonStrategy(ClassComparisonStrategy classComparisonStrategy) {
+		this.classComparisonStrategy = classComparisonStrategy;
+	}
+
+	public File getDebugCacheDirectory() {
+		return debugCacheDirectory;
+	}
+
+	public void setDebugCacheDirectory(File debugCacheDirectory) {
+		this.debugCacheDirectory = debugCacheDirectory;
+	}
+
+	public Class<? extends Annotation> getAttributesAnnotation() {
+		return attributesAnnotation;
+	}
+
+	public void setAttributesAnnotation(Class<? extends Annotation> attributesAnnotation) {
+		this.attributesAnnotation = attributesAnnotation;
+	}
+
+	public Logger getLogger() {
+		return logger;
+	}
+
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+
+	public SerializationStrategy getSerializationStrategy() {
+		return serializationStrategy;
+	}
+
+	public void setSerializationStrategy(SerializationStrategy serializationStrategy) {
+		this.serializationStrategy = serializationStrategy;
+	}
+}
