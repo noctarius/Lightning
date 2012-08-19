e47cdab3060a3c865ba66e9012a61c9cba52d644
Noctarius <me@noctarius.com>
2012-08-19 16:17:57 +0200

    Removed used imports

--- lightning-core/src/test/java/com/github/lightning/PropertyFinderTestCase.java
+++ lightning-core/src/test/java/com/github/lightning/PropertyFinderTestCase.java
@@ -15,9 +15,9 @@
  */
 package com.github.lightning;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
